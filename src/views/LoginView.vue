7e8c2d558f9a3e153c02ef135cd9efc8f0b4c621
Pornchitar Sarsai <64160260@go.buu.ac.th>
2023-01-05 17:31:35 +0700

    Form

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,7 +5,12 @@
         <v-main class="bg-blue-grey-darken-2 pa-16">
             <v-card width="400px" class="mx-auto">
                 <v-card-title primary-title class="text-center"> Login </v-card-title>
-                <v-card-text>Form</v-card-text>
+                <v-card-text>
+                    <v-form>
+                        <v-text-field label="Login Name"></v-text-field>
+                        <v-text-field label="Password" type="password"></v-text-field>
+                    </v-form>
+                </v-card-text>
                 <v-card-actions class="justify-center">
                     <v-btn color="success">Login</v-btn>
                     <v-btn color="error">Clear</v-btn>
