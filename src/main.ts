91c7df5191525da50c0517cd1b3b4799b07e2045
Kaewmajoi <kai84143@gmail.com>
2023-01-06 13:23:43 +0700

    fix git error

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,6 @@ import App from "./App.vue";
 import router from "./router";
 
 import "vuetify/styles";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
-
-import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
-
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
