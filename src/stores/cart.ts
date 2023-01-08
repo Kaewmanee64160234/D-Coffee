7574edcb6b70d08dd638b44c230cd238bfb6711a
64160047 <64160047@go.buu.ac.th>
2023-01-08 21:04:10 +0700

    add function clear and deleteList

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -1,10 +1,22 @@
 import { ref, computed } from "vue";
 import { defineStore } from "pinia";
+import { useLoginStore } from "./login";
 
 export const useCartStore = defineStore("cart", () => {
   const carts: any = ref([]);
+  const loginStore = useLoginStore();
   const addToCard = (item: Object) => {
     carts.value.push(item);
   };
-  return { addToCard, carts };
+ 
+
+  const clear  = () => { 
+    carts.value =  []
+    loginStore.dialog = false ;
+}
+  const deleteList = (id:number) => {
+    carts.value.splice(id, 1);
+  }
+  
+ return { addToCard, carts , clear,deleteList};
 });
